8b0d7b50d7a409853b9523ce1f03c0c2939443f1
Bosh <ohwaitnvm a gmail d com>
2010-11-30 14:34:31 -0500

    initialized rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinguisticExplorer::Application.config.secret_token = 'e7d6e306936e3d63a1e7aa1247817a6d1e1d509fea987500b557bb578c9a130c9003915f79b0cd853ca123d6abbe0c19f2678edd1132a7a2e12c7f2570dc7bc3'
