b3c2805f555ffa94ebd1cc6474b267adfcf0e2b8
Ross Kaffenberger <rosskaff@gmail.com>
2011-02-09 10:29:18 -0500

    WIP refactoring ling search

--- app/presenters/search/ling_search.rb
+++ app/presenters/search/ling_search.rb
@@ -1,18 +1,27 @@
 class LingSearch < Search
 
   def results
-    all_results = Ling.select("lings.name AS ling_name, lings.id AS ling_id")
-
-    all_results = all_results.where("lings.id in (?)", params[:lings]) if params[:lings].present?
+    if show? :ling
+    all_results = Ling.select("lings.name AS ling_name, lings.id AS ling_id").
+      where("lings.id in (?)", selected_ling_ids)
+    end
 
     if show? :property
       all_results = all_results.
         select("properties.name AS prop_name, properties.id AS prop_id").
         joins(:properties).
-        where("lings_properties.property_id" => params[:properties]).
-        includes(:properties)
+        where("lings_properties.property_id" => selected_prop_ids)
     end
 
     all_results
   end
+
+  def selected_ling_ids
+    params[:lings] || Ling.select("id")
+  end
+
+  def selected_prop_ids
+    params[:properties] || Property.select("id")
+  end
+
 end
