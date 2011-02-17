d684613087c90713c8ac2ded07d99b683cc2b04a
Ross Kaffenberger <rosskaff@gmail.com>
2011-02-17 16:32:23 -0500

    refactor search results finder with arel merge

--- app/presenters/search_result_set.rb
+++ app/presenters/search_result_set.rb
@@ -10,19 +10,27 @@ class SearchResultSet
   end
 
   def results
-    all_results = Ling.select("lings.name AS ling_name, lings.id AS ling_id").
-      where("lings.id in (?)", selected_ling_ids)
+    all_results = select_lings.joins(:properties) & select_properties
 
-    all_results = all_results.
-      select("properties.name AS prop_name, properties.id AS prop_id").
-      joins(:properties).
-      where("lings_properties.property_id" => selected_prop_ids)
-    
     if @params[:lings_properties].present?
-      all_results = all_results.where("lings_properties.id" => selected_lings_prop_ids)
+      all_results = all_results & select_lings_properties
     end
     
     all_results
+  end
+  
+  private
+  
+  def select_lings
+    Ling.select("lings.name AS ling_name, lings.id AS ling_id").where("lings.id in (?)", selected_ling_ids)
+  end
+  
+  def select_properties
+    Property.select("properties.name AS prop_name, properties.id AS prop_id").where("properties.id in (?)", selected_prop_ids)
+  end
+  
+  def select_lings_properties
+    LingsProperty.where("lings_properties.id" => selected_lings_prop_ids)
   end
 
   def selected_ling_ids
