56501355ee1e14d151e843873d0c9da0df708248
Ross Kaffenberger <rosskaff@gmail.com>
2011-03-20 22:20:59 -0400

    Intersection filter

--- app/presenters/search_results.rb
+++ app/presenters/search_results.rb
@@ -24,7 +24,7 @@ module SearchResults
 
     depth_0_vals, depth_1_vals = filter_by_lings_prop_params
 
-    depth_0_vals, depth_1_vals = intersect_lings_prop_ids(depth_0_vals, depth_1_vals)
+    depth_0_vals, depth_1_vals = intersect_lings_prop_ids value_pair_params_filter, depth_0_vals, depth_1_vals
 
     depth_0_vals, depth_1_vals = filter_by_all_conditions(depth_0_vals, depth_1_vals, :property_group)
 
@@ -61,18 +61,9 @@ module SearchResults
     @value_pair_params_filter ||= ValuePairParamsFilter.new(params_filter, category_adapter, @params[:lings_props] || {})
   end
 
-  def intersect_lings_prop_ids(depth_0_vals, depth_1_vals)
-    # Calling "to_a" because of bug in rails when calling empty?/any? on relation not yet loaded
-    # Fixed at https://github.com/rails/rails/commit/015192560b7e81639430d7e46c410bf6a3cd9223
-
-    if depth_1_vals.to_a.any?
-      depth_1_vals  = ( LingsProperty.select_ids.with_id(depth_1_vals.map(&:id)).where(:property_id => depth_1_prop_ids) & Ling.parent_ids.with_parent_id(depth_0_vals.map(&:ling_id).uniq))
-
-      depth_0_vals  =   LingsProperty.select_ids.with_id(depth_0_vals.map(&:id)).with_ling_id(depth_1_vals.map(&:parent_id).uniq).where(:property_id => depth_0_prop_ids)
-
-    end
-
-    [depth_0_vals, depth_1_vals]
+  def intersect_lings_prop_ids(filter)
+    intersection_filter = IntersectionFilter.new(filter, depth_0_prop_ids, depth_1_prop_ids)
+    [intersection_filter.depth_0_vals, intersection_filter.depth_1_vals]
   end
 
   def filter_by_all_conditions(depth_0_vals, depth_1_vals, grouping)
@@ -90,9 +81,16 @@ module SearchResults
         depth_1_vals    = depth_1_vals.ling_ids.group(:property_id).having(:property_id => child_prop_ids)
       end
 
-      intersect_lings_prop_ids(depth_0_vals, depth_1_vals)
+      intersect_lings_prop_ids(DummyFilter.new(depth_0_vals, depth_1_vals))
     else
       [depth_0_vals, depth_1_vals]
+    end
+  end
+
+  class DummyFilter
+    attr_reader :depth_0_vals, :depth_1_vals
+    def initialize(depth_0_vals, depth_1_vals)
+      @depth_0_vals, @depth_1_vals = depth_0_vals, depth_1_vals
     end
   end
 
---
+++ app/presenters/search_results/intersection_filter.rb
@@ -0,0 +1,34 @@
+module SearchResults
+  class IntersectionFilter
+    attr_accessor :depth_0_vals, :depth_1_vals
+
+    def initialize(filter, depth_0_prop_ids, depth_1_prop_ids)
+      @depth_0_prop_ids = depth_0_prop_ids
+      @depth_1_prop_ids = depth_1_prop_ids
+      @depth_0_vals, @depth_1_vals = intersect(filter.depth_0_vals, filter.depth_1_vals)
+    end
+
+    private
+
+    def intersect(depth_0_vals, depth_1_vals)
+      # Calling "to_a" because of bug in rails when calling empty?/any? on relation not yet loaded
+      # Fixed at https://github.com/rails/rails/commit/015192560b7e81639430d7e46c410bf6a3cd9223
+      if depth_1_vals.to_a.any?
+        depth_1_vals  = filter_depth_1_vals_by_selected_ling_parents(depth_0_vals, depth_1_vals)
+        depth_0_vals  = filter_depth_0_vals_by_filtered_depth_1_vals(depth_0_vals, depth_1_vals)
+      end
+      [depth_0_vals, depth_1_vals]
+    end
+
+    def filter_depth_1_vals_by_selected_ling_parents(depth_0_vals, depth_1_vals)
+      LingsProperty.select_ids.with_id(depth_1_vals.map(&:id)).where(:property_id => @depth_1_prop_ids) &
+        Ling.parent_ids.with_parent_id(depth_0_vals.map(&:ling_id).uniq)
+    end
+
+    def filter_depth_0_vals_by_filtered_depth_1_vals(depth_0_vals, depth_1_vals)
+      LingsProperty.select_ids.with_id(depth_0_vals.map(&:id)).
+        with_ling_id(depth_1_vals.map(&:parent_id).uniq).where(:property_id => @depth_0_prop_ids)
+    end
+  end
+
+end
