cd1acd9a500272c6b531e15060435c41e58a4a0c
Bosh@MF <ohwaitnvm at gmail>
2011-04-07 02:17:53 -0400

    fixed seeding for elpvs

--- app/models/example.rb
+++ app/models/example.rb
@@ -8,6 +8,8 @@ class Example < ActiveRecord::Base
   has_many :examples_lings_properties, :dependent => :destroy
   has_many :lings_properties, :through => :examples_lings_properties
 
+  scope :in_group, lambda { |group| where(:group => group) }
+
   def group_association_match
     errors.add(:ling, "#{group.ling_name_for_depth(ling.depth)} must belong to the same group as this #{group.example_name}") if ling && (ling.group != group)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
 require 'csv'
-
 logger = Logger.new(STDOUT)
 
-user_list   = {}
-ling_list   = {}
-cat_list    = {}
-prop_list   = {}
+cat_list  = {}
+user_list = {}
+ling_list = {}
+prop_list = {}
+example_list = {}
+lings_property_list = {}
 
 def ling_name(name)
   "ling #{name}".titleize
@@ -26,7 +26,6 @@ def category_name(name)
   name.titleize
 end
 
-puts "Loading data"
 puts "Starting Users..."
 # Create Users(id,name,email,accesslevel)
 CSV.foreach(Rails.root.join("doc", "data", "User.csv"), :headers => true) do |row|
@@ -36,7 +35,7 @@ CSV.foreach(Rails.root.join("doc", "data", "User.csv"), :headers => true) do |ro
   next if user.present?
 
   user = User.new(
-      :name     => row["name"],
+      :name => row["name"],
       :password => "hunter2",
       :password_confirmation => "hunter2"
   )
@@ -61,14 +60,14 @@ CSV.foreach(Rails.root.join("doc", "data", "GroupMembership.csv"), :headers => t
   group = Group.find_by_name(group_name(row["group_id"]))
   Membership.create(:user => user, :group => group, :level => row["level"])
 end
-puts "Done with Memberships, starting Lings"
 
+puts "Done with Memberships, starting Lings"
 # Create Lings(id,name,parentid,depth,group,creator,timestamp)
 CSV.foreach(Rails.root.join("doc", "data", "Ling.csv"), :headers => true) do |row|
   name = ling_name(row["name"])
   ling = Ling.find_or_initialize_by_name(name) do |l|
-    l.depth   = row["depth"]
-    l.group   = Group.find_by_name(group_name(row["group"]))
+    l.depth = row["depth"]
+    l.group = Group.find_by_name(group_name(row["group"]))
   end
   ling_list[row["id"]] = name
   ling.save!
@@ -79,8 +78,8 @@ puts "Done with Lings, starting ling parent associations"
 CSV.foreach(Rails.root.join("doc", "data", "Ling.csv"), :headers => true) do |row|
   next if row["parentid"].blank?
 
-  child   = Ling.find_by_name(ling_name(row["name"]))
-  parent  = Ling.find_by_name(ling_name(row["parentid"]))
+  child = Ling.find_by_name(ling_name(row["name"]))
+  parent = Ling.find_by_name(ling_name(row["parentid"]))
   child.parent = parent
 
   begin
@@ -96,10 +95,10 @@ end
 puts "Done with Ling parents, starting Categories"
 # Create Categories(id,name,depth,group,creator,timestamp)
 CSV.foreach(Rails.root.join("doc", "data", "Category.csv"), :headers => true) do |row|
-  name     = category_name(row["name"])
+  name = category_name(row["name"])
   category = Category.find_or_initialize_by_name(name)
-  category.depth     = row["depth"]
-  category.group     = Group.find_by_name(group_name(row["group"]))
+  category.depth = row["depth"]
+  category.group = Group.find_by_name(group_name(row["group"]))
   cat_list[row["name"]] = name
 
   category.save!
@@ -108,10 +107,10 @@ end
 puts "Done with Categories, starting Properties"
 # Create Properties(id,name,category,group,creator,timestamp)
 CSV.foreach(Rails.root.join("doc", "data", "Property.csv"), :headers => true) do |row|
-  name     = prop_name(row["name"])
+  name = prop_name(row["name"])
   property = Property.find_or_initialize_by_name(name)
-  property.category  = Category.find_by_name(cat_list[row["category"]])
-  property.group     = Group.find_by_name(group_name(row["group"]))
+  property.category = Category.find_by_name(cat_list[row["category"]])
+  property.group = Group.find_by_name(group_name(row["group"]))
   prop_list[row["id"]] = name
 
   property.save!
@@ -129,8 +128,8 @@ puts "Done with Properties, starting LingsProperties"
 # Create LingsProperties(id,lingid,propid,value,group,creator,timestamp)
 CSV.foreach(Rails.root.join("doc", "data", "LingPropVal.csv"), :headers => true) do |row|
   group = Group.find_by_name(group_name(row["group"]))
-  ling  = Ling.in_group(group).find_by_name(ling_list[row["lingid"]])
-  prop  = Property.in_group(group).find_by_name(prop_list[row["propid"]])
+  ling = Ling.in_group(group).find_by_name(ling_list[row["lingid"]])
+  prop = Property.in_group(group).find_by_name(prop_list[row["propid"]])
 
   attributes = {
     :ling_id      => ling.id,
@@ -138,12 +137,51 @@ CSV.foreach(Rails.root.join("doc", "data", "LingPropVal.csv"), :headers => true)
     :value        => MEANINGFUL_VALUES[row["value"]]
   }
 
-  next if LingsProperty.where(attributes).first.present?
-  lp          = LingsProperty.new(attributes)
-
-  lp.group    = ling.group
-  lp.save!
+  already_exists = LingsProperty.where(attributes).first
+  if already_exists.present?
+    lings_property_list[row["id"]] = already_exists.id
+  else
+    lp = LingsProperty.new(attributes)
+    lp.group = ling.group
+    lp.save!
+    lings_property_list[row["id"]] = lp.id
+  end
 end
 
-puts "Done with LingsProperties"
+puts "Done with LingsProperties, starting Examples"
+# Create Examples(id,lingid,group,creator,timestamp)
+number = 0
+CSV.foreach(Rails.root.join("doc", "data", "Example.csv"), :headers => true) do |row|
+  number += 1
+  name = number.to_s
+  ling = Ling.find_by_name(ling_name(row["lingid"]))
+  group = Group.find_by_name(group_name(row["group"]))
+
+  example = Example.new(:name => name)
+  example.ling = ling
+  example.group = group
+  example.save!
+  example_list[row["id"].to_s] = example.id
+end
+
+puts "Done with Examples, starting ExamplesLingsPropsVals"
+# Create ExamplesLingsProperties(id,exampleid,LingPropValID,group,creator,timestamp)
+CSV.foreach(Rails.root.join("doc", "data", "ExampleLingPropVal.csv"), :headers => true) do |row|
+  group = Group.find_by_name(group_name(row["group"]))
+  example = Example.in_group(group).find(example_list[row["exampleid"]])
+  lp_id = lings_property_list[row["LingPropValID"].to_s]
+  lings_property = LingsProperty.in_group(group).find(lp_id)
+  attributes = {
+    :example_id => example.id,
+    :lings_property_id => lings_property.id,
+  }
+  next if ExamplesLingsProperty.where(attributes).first.present?
+
+  elp = ExamplesLingsProperty.new(attributes)
+  elp.group = group
+  elp.save!
+end
+
+puts "Done with ExamplesLingsPropsVals"
+
 puts "Seeding complete!"
