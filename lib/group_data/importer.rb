0d362945f6b8a35eee18aee8d5f8189e093d5310
Ross Kaffenberger <rosskaff@gmail.com>
2011-05-05 07:05:14 -0400

    Group data importer: csv spec in comments

---
+++ lib/group_data/importer.rb
@@ -0,0 +1,39 @@
+# GroupDataImporter
+# 
+# ==> Category.csv <==
+# id,name,depth,group_id,creator_id,description
+# 
+# ==> Example.csv <==
+# id,ling_id,group_id,creator_id,name
+# 
+# ==> ExampleLingsProperty.csv <===
+# id,example_id,lings_property_id,group_id,creator_id
+# 
+# ==> Group.csv <==
+# id, name, privacy, depth_maximum, ling0_name, ling1_name, property_name, category_name, lings_property_name, example_name, examples_lings_property_name, example_fields
+# 
+# ==> Ling.csv <==
+# id,name,parent_id,depth,group_id,creator_id
+# 
+# ==> LingsProperty.csv <==
+# id,ling_id,prop_id,value,group_id,creator_id
+# 
+# ==> Membership.csv <==
+# id,user_id,group_id,level,creator_id
+# 
+# ==> Property.csv <==
+# id,name,description,category,group_id,creator_id
+# 
+# ===> StoredValue.csv <=====
+# id, storable_id, storable_type, key, value, group_id
+# 
+# ==> User.csv <==
+# id,name,email,accesslevel, password
+# 
+
+module GroupData
+  class Importer
+    
+  end
+  
+end
---
+++ spec/lib/group_data/importer_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module GroupData
+  describe Importer do
+  
+  end
+end
