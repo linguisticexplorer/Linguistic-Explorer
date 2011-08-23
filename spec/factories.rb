1e8178360e8b0865704bf2e8d23f9e5a4ac5772c
Marco Liberati <marco.liberati@hotmail.it>
2011-08-23 11:38:49 -0400

    Update syntax for new version of FactoryGirl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,60 +1,84 @@
-Factory.define :group do |f|
-  f.name           "The Best Group"
-  f.privacy        Group::PUBLIC
-  f.depth_maximum  Group::MAXIMUM_ASSIGNABLE_DEPTH
-  f.example_fields "gloss, number"
+FactoryGirl.define do
+  factory :group do |f|
+    f.name           "The Best Group"
+    f.privacy        Group::PUBLIC
+    f.depth_maximum  Group::MAXIMUM_ASSIGNABLE_DEPTH
+    f.example_fields "gloss, number"
+  end
 end
 
-Factory.define :user do |f|
-  f.name          "Bob Jones"
-  f.email         "bob@example.com"
-  f.access_level  "user"
-  f.password      "password"
+FactoryGirl.define do
+  factory :user do |f|
+    f.name          "Bob Jones"
+    f.email         "bob@example.com"
+    f.access_level  "user"
+    f.password      "password"
+  end
 end
 
-Factory.define :ling do |f|
+FactoryGirl.define do
+  factory :ling do |f|
   f.name "English"
   f.depth 0
   f.association :group, :factory => :group
+  end
 end
 
-Factory.define :membership do |f|
+FactoryGirl.define do
+  factory :membership do |f|
   f.level "member"
   f.association :group, :factory => :group
   f.association :member, :factory => :user
+  end
 end
 
-Factory.define :property do |f|
+FactoryGirl.define do
+  factory :property do |f|
   f.name "Adjective"
 # NOTE: You must pass the following two yourself because the category must belong to the same group as the created property
 #  f.association :category, :factory => :category
 #  f.association :group, :factory => :group
+  end
 end
 
-Factory.define :category do |f|
+FactoryGirl.define do
+  factory :category do |f|
   f.name "Grammar"
   f.depth 0
   f.association :group, :factory => :group
+  end
 end
 
-Factory.define :lings_property do |f|
+FactoryGirl.define do
+  factory :lings_property do |f|
+  end
 end
 
-Factory.define :example do |f|
+FactoryGirl.define do
+  factory :example do |f|
   f.name "SampleExample"
 # NOTE: You must pass the following two yourself because the ling must belong to the same group as the created example
 #  f.association :group, :factory => :group
 #  f.association :ling, :factory => :ling
+  end
 end
 
-Factory.define :examples_lings_property do |f|
+FactoryGirl.define do
+  factory :examples_lings_property do |f|
+
+  end
 end
 
-Factory.define :search do |f|
+FactoryGirl.define do
+  factory :search do |f|
   f.name "New Search"
   f.association :group, :factory => :group
   f.association :creator, :factory => :user
+  end
 end
 
-Factory.define :stored_value do |f|
+FactoryGirl.define do
+  factory :stored_value do |f|
+
+  end
 end
