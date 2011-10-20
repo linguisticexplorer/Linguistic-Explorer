10b606db663491e89f4f0714eafdecda02c25899
Marco Liberati <marco.liberati@hotmail.it>
2011-10-20 12:42:26 -0400

    Show Loading while browsing in results within AJAX history

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -2,10 +2,13 @@
 jQuery.noConflict();
 
 jQuery(function() {
+
+    // Text and image while loading
+    var img = "<img src='/images/loader.gif' class='loading'/>"
+    var text = "<span class='loading'>Page is loading...</span>"
+
     // Manage the AJAX pagination and changing the URL
     jQuery(".pagination a").live("click", function(e) {
-        var img = "<img src='/images/loader.gif' class='loading'/>"
-        var text = "<span class='loading'>Page is loading...</span>"
         //jQuery.setFragment({ "page" : jQuery.queryString(this.href).page })
         jQuery.getScript(this.href);
         jQuery(".pagination").html(text+img);
@@ -13,8 +16,9 @@ jQuery(function() {
         e.preventDefault();
     });
 
-    // Let navigate the browser thru the AJAX history
+    // Let navigate the browser throught the AJAX history
     jQuery(window).bind("popstate", function() {
         jQuery.getScript(location.href);
+        jQuery(".pagination").html(text+img);
     });
 });
