b54295c53aaf9ce60f3c16f88e479861a904d6f4
Marco Liberati <marco.liberati@hotmail.it>
2011-11-10 13:46:53 -0500

    New helper methods for Cross results

--- app/helpers/search_results_helper.rb
+++ app/helpers/search_results_helper.rb
@@ -10,6 +10,11 @@ module SearchResultsHelper
     "<a href='/groups/#{current_group.to_param}/properties/#{property.to_param}'>#{h(property.name)}</a>".html_safe
   end
 
+  def link_to_cross_lings(parent, lings)
+    return lings.count if lings.empty?
+    link_to lings.count, :action => "cross_lings", :search => @search.query, :cross_ids => parent.map(&:id)
+  end
+
   def row_methods
     @row_methods ||= {
       :ling_0         => lambda { |v| link_to_ling(v.ling) },
@@ -22,7 +27,7 @@ module SearchResultsHelper
       :example_1      => lambda { |v| v.examples.map(&:name).join(", ") },
       :cross_property => lambda { |v| link_to_property(v.property) },
       :cross_value    => lambda { |v| v.value },
-      :count          => lambda { |v| v.count }
+      :count          => lambda { |parent, v| link_to_cross_lings(parent, v) }
     }
   end
 
@@ -39,6 +44,13 @@ module SearchResultsHelper
       attrs[:class] = "search_result row"
       attrs["data-parent-value"] = entry.parent.inject(0) {|sum, lp| sum + lp.id}
       attrs["data-child-value"] = "#{attrs["data-parent-value"]}-#{entry.child.count}"
+    end
+  end
+
+  def search_result_attributes_for_ling_cross(entry)
+    {}.tap do |attrs|
+      attrs[:class] = "search_result row"
+      attrs["data-parent-value"] = entry.id
     end
   end
 
