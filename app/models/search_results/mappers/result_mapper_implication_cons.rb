bb3d551d6174231d70b88e70c4cc2affa3cc2b3f
Marco Liberati <marco.liberati@hotmail.it>
2011-12-03 01:08:03 -0500

    Cleaned from no more useful methods

--- app/models/search_results/mappers/result_mapper_implication_cons.rb
+++ app/models/search_results/mappers/result_mapper_implication_cons.rb
@@ -10,18 +10,6 @@ module SearchResults
         LingsProperty.in_group(group).select("ling_id").where("property_value" => prop_value).index_by(&:ling_id).keys
       end
 
-      def self.lings_subset(ling_ids)
-        LingsProperty.with_ling_id(ling_ids).group_by(&:ling_id)
-      end
-
-      def self.vals_by_prop_values_filtered(to_be_filtered, accepted_values)
-        vals_by_prop_values(to_be_filtered).reject {|prop_val,lings| !accepted_values.include?(prop_val)}
-      end
-
-      def self.all_prop_values(ling_ids)
-        LingsProperty.with_ling_id(ling_ids).select("DISTINCT(property_value), id").group_by(&:property_value)
-      end
-
       def self.find_antecedents(ids)
         prop_values_selected = LingsProperty.select_ids.group(:property_value).having(["COUNT(property_value) <= ?", ids.size+1]).map(&:property_value)
         LingsProperty.select_ids.where(:property_value => prop_values_selected).group_by(&:property_value)
