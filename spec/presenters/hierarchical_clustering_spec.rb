214596f5594e99821dae57c31002bf5dee1c2042
Marco Liberati <marco.liberati@hotmail.it>
2012-01-25 01:38:15 -0500

    Fixed output format for hierarchical clustering spec

--- spec/presenters/hierarchical_clustering_spec.rb
+++ spec/presenters/hierarchical_clustering_spec.rb
@@ -59,56 +59,48 @@ describe HierarchicalClustering do
   describe "calculate a hierarchical cluster" do
     before(:each) do
       @clusterer = HierarchicalClustering.new({"one" => [0,0]})
+      @expected_string = "{\"name\": 'one' ,\"coords\": \"[0, 0]\" }"
     end
 
     describe "with one point and average linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:euclidean).to_s.should == expected_string
+        @clusterer.cluster(:euclidean).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:manhattan).to_s.should == expected_string
+        @clusterer.cluster(:manhattan).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:max).to_s.should == expected_string
+        @clusterer.cluster(:max).to_s.should == @expected_string
       end
     end
 
     describe "with one point and single linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:euclidean, :single).to_s.should == expected_string
+        @clusterer.cluster(:euclidean, :single).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:manhattan, :single).to_s.should == expected_string
+        @clusterer.cluster(:manhattan, :single).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:max, :single).to_s.should == expected_string
+        @clusterer.cluster(:max, :single).to_s.should == @expected_string
       end
     end
 
     describe "with one point and complete linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:euclidean, :complete).to_s.should == expected_string
+        @clusterer.cluster(:euclidean, :complete).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:manhattan, :complete).to_s.should == expected_string
+        @clusterer.cluster(:manhattan, :complete).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }"
-        @clusterer.cluster(:max, :complete).to_s.should == expected_string
+        @clusterer.cluster(:max, :complete).to_s.should == @expected_string
       end
     end
   end
@@ -116,55 +108,47 @@ describe HierarchicalClustering do
   describe "calculate a hierarchical cluster by distances" do
     before(:each) do
       @clusterer = HierarchicalClustering.new({"one" => [0,0], "two" => [1,1]})
+      @expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": 'one' ,\"coords\": \"[0, 0]\" }, {\"name\": 'two' ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
     end
 
     describe "with two point and average linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster.to_s.should == expected_string
+        @clusterer.cluster.to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:manhattan).to_s.should == expected_string
+        @clusterer.cluster(:manhattan).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:max).to_s.should == expected_string
+        @clusterer.cluster(:max).to_s.should == @expected_string
       end
     end
     describe "with two point and single linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:euclidean, :single).to_s.should == expected_string
+        @clusterer.cluster(:euclidean, :single).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:manhattan, :single).to_s.should == expected_string
+        @clusterer.cluster(:manhattan, :single).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:max, :single).to_s.should == expected_string
+        @clusterer.cluster(:max, :single).to_s.should == @expected_string
       end
     end
 
     describe "with two point and complete linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:euclidean, :complete).to_s.should == expected_string
+        @clusterer.cluster(:euclidean, :complete).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:manhattan, :complete).to_s.should == expected_string
+        @clusterer.cluster(:manhattan, :complete).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }"
-        @clusterer.cluster(:max, :complete).to_s.should == expected_string
+        @clusterer.cluster(:max, :complete).to_s.should == @expected_string
       end
 
     end
@@ -173,56 +157,48 @@ describe HierarchicalClustering do
   describe "calculate a hierarchical cluster by linkages" do
     before(:each) do
       @clusterer = HierarchicalClustering.new({"one" => [0,0], "two" => [1,1], "three" => [0,2]})
+      @expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": 'one' ,\"coords\": \"[0, 0]\" }, {\"name\": 'two' ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": 'three' ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
     end
 
     describe "with more points with average linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:euclidean).to_s.should == expected_string
+        @clusterer.cluster(:euclidean).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:manhattan).to_s.should == expected_string
+        @clusterer.cluster(:manhattan).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:max).to_s.should == expected_string
+        @clusterer.cluster(:max).to_s.should == @expected_string
       end
     end
 
     describe "with more points with single linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:euclidean, :single).to_s.should == expected_string
+        @clusterer.cluster(:euclidean, :single).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:manhattan, :single).to_s.should == expected_string
+        @clusterer.cluster(:manhattan, :single).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:max, :single).to_s.should == expected_string
+        @clusterer.cluster(:max, :single).to_s.should == @expected_string
       end
     end
 
     describe "with more points with complete linkage:" do
       it "compute cluster with euclidean distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:euclidean, :complete).to_s.should == expected_string
+        @clusterer.cluster(:euclidean, :complete).to_s.should == @expected_string
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:manhattan, :complete).to_s.should == expected_string
+        @clusterer.cluster(:manhattan, :complete).to_s.should == @expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2]\" }], \"size\": 3 }"
-        @clusterer.cluster(:max, :complete).to_s.should == expected_string
+        @clusterer.cluster(:max, :complete).to_s.should == @expected_string
       end
     end
   end
@@ -232,20 +208,18 @@ describe HierarchicalClustering do
   describe "with more points with average linkage in a 5th dimensional spacein a 5th dimensional space:" do
     before(:each) do
       @clusterer = HierarchicalClustering.new({"one" => [0, 0, 0, 0, 0], "two" => [1,1,1,1,1], "three" => [0,2,2,2,2]})
+      @expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": 'one' ,\"coords\": \"[0, 0, 0, 0, 0]\" }, {\"name\": 'two' ,\"coords\": \"[1, 1, 1, 1, 1]\" }], \"size\": 2 }, {\"name\": 'three' ,\"coords\": \"[0, 2, 2, 2, 2]\" }], \"size\": 3 }"
     end
     it "compute cluster with euclidean distance" do
-      expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0, 0, 0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1, 1, 1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2, 2, 2, 2]\" }], \"size\": 3 }"
-      @clusterer.cluster(:euclidean).to_s.should == expected_string
+      @clusterer.cluster(:euclidean).to_s.should == @expected_string
     end
 
     it "compute cluster with manhattan distance" do
-      expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0, 0, 0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1, 1, 1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2, 2, 2, 2]\" }], \"size\": 3 }"
-      @clusterer.cluster(:manhattan).to_s.should == expected_string
+      @clusterer.cluster(:manhattan).to_s.should == @expected_string
     end
 
     it "compute cluster with max distance" do
-      expected_string = "{ \"name\": \"one-two-three\", \"children\": [{ \"name\": \"one-two\", \"children\": [{\"name\": \"one\" ,\"coords\": \"[0, 0, 0, 0, 0]\" }, {\"name\": \"two\" ,\"coords\": \"[1, 1, 1, 1, 1]\" }], \"size\": 2 }, {\"name\": \"three\" ,\"coords\": \"[0, 2, 2, 2, 2]\" }], \"size\": 3 }"
-      @clusterer.cluster(:max).to_s.should == expected_string
+      @clusterer.cluster(:max).to_s.should == @expected_string
     end
   end
 
@@ -263,12 +237,12 @@ describe HierarchicalClustering do
       end
 
       it "compute cluster with manhattan distance" do
-        expected_string = "((one:5, two:5):6, three:6):0.1"
+        expected_string = "((one:5, two:5):6, three:6):0.1;"
         @clusterer.cluster(:manhattan).to_s.should == expected_string
       end
 
       it "compute cluster with max distance" do
-        expected_string = "((one:1, two:1):1, three:1):0.1"
+        expected_string = "((one:1, two:1):1, three:1):0.1;"
         @clusterer.cluster(:max).to_s.should == expected_string
       end
     end
