fd18dbe920d7660632d674739ca09823aa59571e
Marco@DebianVM <marco@terraling.com>
2012-11-10 17:09:24 +0000

    Fixed jquery conflicts

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,24 +1,22 @@
-// To avoid conflict with Prototype library
-jQuery.noConflict();
+/* On DOM loaded */
+$(function() {
 
-jQuery(function() {
+  // Text and image while loading
+  var img = "<img src='/images/loader.gif' class='loading'/>"
+  var text = "<span class='loading'>Page is loading...</span>"
 
-    // Text and image while loading
-    var img = "<img src='/images/loader.gif' class='loading'/>"
-    var text = "<span class='loading'>Page is loading...</span>"
+  // Manage the AJAX pagination and changing the URL
+  $(".pagination a").live("click", function(e) {
+      //jQuery.setFragment({ "page" : jQuery.queryString(this.href).page })
+      jQuery.getScript(this.href);
+      jQuery(".pagination").html(text+img);
+      history.pushState(null, document.title, this.href);
+      e.preventDefault();
+  });
 
-    // Manage the AJAX pagination and changing the URL
-    jQuery(".pagination a").live("click", function(e) {
-        //jQuery.setFragment({ "page" : jQuery.queryString(this.href).page })
-        jQuery.getScript(this.href);
-        jQuery(".pagination").html(text+img);
-        history.pushState(null, document.title, this.href);
-        e.preventDefault();
-    });
-
-    // Let navigate the browser throught the AJAX history
-    jQuery(window).bind("popstate", function() {
-        jQuery.getScript(location.href);
-        jQuery(".pagination").html(text+img);
-    });
+  // Let navigate the browser throught the AJAX history
+  $(window).bind("popstate", function() {
+      $.getScript(location.href);
+      $(".pagination").html(text+img);
+  });
 });
