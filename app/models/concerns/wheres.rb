9d4244d1f3fbfaab94a251c7b824141a1557c8ab
Marco@DebianVM <marco@terraling.com>
2013-03-03 15:19:34 +0000

    Having finally working Squeel

--- app/models/concerns/wheres.rb
+++ app/models/concerns/wheres.rb
@@ -2,8 +2,8 @@ module Concerns
   module Wheres
     def self.included(base)
       base.class_exec do
-        scope :in_group, lambda { |group| where(:group_id => group.id) }
-        scope :at_depth, lambda { |depth| where(:depth => depth) }
+        scope :in_group, lambda { |group| where{ :group == group } }
+        scope :at_depth, lambda { |depth| where{ :depth == depth } }
       end
     end
   end
