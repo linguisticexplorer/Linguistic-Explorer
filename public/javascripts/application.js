a577b0dd5498e007270eaee6baf080a47edc16f7
lingz <lz781@nyu.edu>
2013-06-17 15:20:13 -0400

    bootstrap navbar complete

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -50,6 +50,29 @@ $(function() {
     });
 });
 
+/*menu handler*/
+$(function(){
+  function stripTrailingSlash(str) {
+    if(str.substr(-1) == '/') {
+      return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+
+  var url = window.location.pathname;  
+  var activePage = stripTrailingSlash(url);
+
+  $('.nav li a').each(function(){  
+    var currentPage = stripTrailingSlash($(this).attr('href'));
+
+    if (activePage == currentPage) {
+      $(this).parent().addClass('active'); 
+    } 
+  });
+});
+
+
+
 /* Function to hide a div by id */
 function hide_div(id){
     $(id).hide('slow');
