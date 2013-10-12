25b3130313b1197e3c795b66a22f5a74ac43f0b3
dej611 <marco.liberati@hotmail.it>
2013-10-12 17:19:11 +0100

    Fix for Version increasing task

--- app/helpers/version.rb
+++ app/helpers/version.rb
@@ -25,7 +25,18 @@ module Version
 	  	
 	  	current = YAML.load_file(TARGET)
 	  	puts "Incrementing #{fieldname} value from #{Integer(current[fieldname])} to #{Integer(current[fieldname]) + 1}" unless fieldname.nil?
-      current[fieldname] = "#{Integer(current[fieldname]) + 1}" unless fieldname.nil?
+      # Take care of reseting minor and patch entries when bumping...
+      case fieldname
+      when 'major'
+        current['major'] = "#{Integer(current[fieldname]) + 1}"
+        current['minor'] = '0';
+        current['patch'] = '0';
+      when 'minor'
+        current['minor'] = "#{Integer(current[fieldname]) + 1}"
+        current['patch'] = '0';
+      else
+        current[fieldname] = "#{Integer(current[fieldname]) + 1}"
+      end
 
 	    File.open(TARGET, 'w') { |f| YAML.dump(current, f) }
 	    puts "New Version set to: #{current['major'] + "." + current['minor'] + "." + current['patch'] + '-' + current['build']}"
