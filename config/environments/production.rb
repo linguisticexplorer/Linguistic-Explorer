cb8dd4e508e60003597d1ef302f8ffd029fc0185
dej611 <marco.liberati@hotmail.it>
2014-09-14 12:29:02 +0100

    Add alpha_paginate to the precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ LinguisticExplorer::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile += %w( alphabetical_paginate.js )
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
