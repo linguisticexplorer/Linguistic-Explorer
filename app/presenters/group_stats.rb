88b0994d3356093d0bd9386445f5aca9aa54f667
dej611 <marco.liberati@hotmail.it>
2014-09-25 21:12:27 +0100

    Uniformed style + added group stats

--- app/presenters/group_stats.rb
+++ app/presenters/group_stats.rb
@@ -1,16 +1,9 @@
 module GroupStats
 
-  attr_accessor :lings_info, :props_info
-
-  # Infos preload returning self for chaining
-  def loadInfos
-    @lings_info, @props_info = {}, {}
-    # lings_with_all_property_quotas || props_with_all_ling_quotas
-    self
-  end
-
-  def lings_in_group(depth)
-    self.lings.at_depth(depth).count(:id)
+  def lings_in_group(depth = 0)
+    @lings_at_depth = Hash.new if @lings_at_depth.nil?
+    @lings_at_depth[depth] ||= self.lings.at_depth(depth)
+    @lings_at_depth[depth].count(:id)
   end
 
   def props_in_group
@@ -18,7 +11,7 @@ module GroupStats
   end
 
   def ling_props_in_group
-    @lings_prop_total ||= LingsProperty.in_group(self).count(:id)
+    @lings_prop_total ||= self.lings_properties.count(:id)
   end
 
   def examples_in_group
@@ -29,30 +22,17 @@ module GroupStats
     @mem_total ||= Membership.where(:group_id => self.id).count(:id)
   end
 
-  def lings_with_all_property_quotas
-    @lings_info.tap do |lings|
-      (0..10).each do |step|
-        lings[step*10] ||= lings_with_property_quota(step*10).size
-      end
-    end
+  def lings_with_property_quota(depth)
+    self.lings_properties.
+    # where(:ling_id => @lings_at_depth[depth]).
+    group(:ling_id).
+    having(["COUNT(property_id) >= ?", props_in_group]).count.size
   end
 
-  def props_with_all_ling_quotas
-    @props_info.tap do |props|
-      (0..10).each do |step|
-        props[step*10] ||= props_with_lings_quota(step*10).size
-      end
-    end
-  end
-
-  private
-
-  def lings_with_property_quota(quota=90)
-    self.lings_properties.group(:ling_id).having(["COUNT(property_id) >= ?", props_in_group * quota / 100]).count
-  end
-
-  def props_with_lings_quota(quota=90)
-    LingsProperty.in_group(self).group(:property_id).having(["COUNT(ling_id) >= ?", lings_in_group * quota / 100]).count
+  def props_with_lings_quota
+    self.lings_properties.
+      group(:property_id).having(["COUNT(ling_id) >= ?", lings_in_group]).
+      count.size
   end
 
 
