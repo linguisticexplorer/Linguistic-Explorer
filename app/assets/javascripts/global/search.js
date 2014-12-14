fea3432a7b7675274c5ce2db303acd26810313c8
dej611 <marco.liberati@hotmail.it>
2014-12-14 15:55:27 +0000

    Fixed an issue with the quick search

--- app/assets/javascripts/global/search.js
+++ app/assets/javascripts/global/search.js
@@ -48,7 +48,7 @@
   function createMatcher(type, group, resolver){
 
     function defaultResolver(entry){
-      return {name: entry[type].name.replace(/\\/g, '').replace(/\_/, ' '), id: entry[type].id, type: type };
+      return {name: entry[type].name.replace(/\\/g, '').replace(/\_/, ' '), id: entry[type].id, type: types[type] };
     }
 
     function dataTransform(list){
