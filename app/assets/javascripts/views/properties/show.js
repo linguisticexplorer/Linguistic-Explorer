626bcdec2ea350400374f88041da2b7699d1bd0b
dej611 <marco.liberati@hotmail.it>
2015-03-01 13:43:31 +0000

    Added new modal to add values from props

--- app/assets/javascripts/views/properties/show.js
+++ app/assets/javascripts/views/properties/show.js
@@ -8,13 +8,21 @@
     var properties = this.Terraling.Properties;
 
     properties.show = {
-        init: setupAnalysis
+        init: setupPage
     };
 
     var resourceId = 'property';
     var currentId, currentName,
         resourceTemplate,
         resourcesDict;
+
+    function setupPage(){
+      setupAnalysis();
+
+      if($('#addValue').length){
+        setupAddValueModal();
+      }
+    }
 
     function setupAnalysis(){
 
@@ -30,7 +38,7 @@
       resourceTemplate = HoganTemplates[tplPath];
 
       // init the typeahead
-      setupTypeahead();
+      setupTypeahead('property', propertyResolver, onItemSelected);
 
       $(document)
         .on('click', '.remove-items', removeProperties)
@@ -42,23 +50,42 @@
 
     function loadMap(){
       var options = {
-        name: $('#details').data('id'),
+        name: currentId,
         type: 'property'
       };
       T.Visualization.Map.init('property-map', options);
     }
 
-    function setupTypeahead(){
+    function setupTypeahead(type, resolver, onSelection){
 
       T.Search.quickTemplate(
-        resourceId,
-        {type: 'property', template: 'resource'},
-        {nameResolver: nameResolver, onSelection: onItemSelected}
+        type,
+        {type: type, template: 'resource'},
+        {nameResolver: resolver, onSelection: onSelection}
       );
     }
 
-    function nameResolver(){
+    function propertyResolver(){
       return T.groups[T.currentGroup].property_name;
+    }
+
+    function setupAddValueModal(){
+      $('#lingsModal').modal({show: false});
+      setupTypeahead('ling', lingResolver, onLingSelected);
+
+      $('body').on('click', '#addValue', function(){
+        $('#lingsModal').modal('toggle');
+      });
+    }
+
+    function lingResolver(){
+      return T.groups[T.currentGroup].ling0_name;
+    }
+
+    function onLingSelected(evt, ling, name){
+      // go to the supported edit page within the right anchor
+      var params = '?commit=Select&prop_id='+currentId+'#value-select';
+      window.location.href = '/groups/'+T.currentGroup+'/lings/'+ling.id+'/supported_set_values'+params;
     }
 
     function onItemSelected(evt, prop, name){
