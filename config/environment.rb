8fa9ded26c56794de31ac3738525ac3d0e365a05
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-09-27 14:40:28 -0400

    fixed and refactored pagination

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-unless defined? DEFAULT_PER_PAGE
+unless defined? ActiveRecord::Base.per_page
   # see: http://groups.google.com/group/will_paginate/browse_thread/thread/eda47114e3127709
   ActiveRecord::Base.instance_eval do
     def per_page; 25; end
   end
-  DEFAULT_PER_PAGE = ActiveRecord::Base.per_page
 end
 
 # Initialize the rails application
