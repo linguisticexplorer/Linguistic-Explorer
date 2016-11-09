53720dad08171a1700cbe57264964e2b5ec615a4
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-11-09 18:42:27 -0500

    added and refactored user_steps.rb

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -3,31 +3,59 @@ Given /^I am a visitor$/ do
 end
 
 Given /^I am signed in as a ([^ ]+) of (.*)/ do |membership_level, group|
-  password  = "hunter2"
-  email = "e@mail.com"
-  level = (Membership::ADMIN == membership_level ? membership_level : Membership::MEMBER )
+  password = "hunter2"
+  email    = "e@mail.com"
+  level    = (Membership::ADMIN == membership_level ? membership_level : Membership::MEMBER )
+  options  = {
+    group: group,
+    level: level
+  }
 
-  @user = create_user(:email => email, :password => password)
-  @group = Group.find_by_name(group) || FactoryGirl.create(:group, :name => group)
-  Membership.create!(:member => @user, :group => @group, :level => level)
+  log_in(email, password, options)
+end
 
-  visit path_to("the home page")
-  click_link "Sign in"
-  fill_in "Email",    :with => email
-  fill_in "Password", :with => password
-  click_button "Sign in"
+Given /^I am signed in as an expert member of (.*) for lings:?$/ do |group, table|
+  email    = "e@mail.com"
+  password = "hunter2"
+  options  = {
+    group: group,
+    level: Membership::MEMBER
+  }
+
+  membership = sign_up(email, password, options)
+
+  add_membership2lings_role_from_table(membership, :expert, table)
+
+  sign_in(email, password)
 end
 
 Given /^I am signed in as "(.*)"/ do |email|
   password  = "hunter2"
-  @user     = User.find_by_email(email) || create_user(:email => email, :password => password)
 
-  visit path_to("the home page")
-  click_link "Sign in"
+  log_in(email, password)
+end
 
-  fill_in "Email",    :with => email
-  fill_in "Password", :with => password
-  click_button "Sign in"
+Given /^a user with email "([^\"]+)" is a ([^ ]+) of (.*)/ do |email, membership_level, group|
+  password  = "hunter2"
+  options   = {
+    group: group,
+    level: Membership::ADMIN == membership_level ? membership_level : Membership::MEMBER
+  }
+
+  sign_up(email, password, options)
+end
+
+Given /^a user with email "([^\"]+)" is an expert member of (.*) for lings:?$/ do |email, group, table|
+  password  = "hunter2"
+  options   = {
+    group: group,
+    level: Membership::MEMBER
+  }
+
+  membership = sign_up(email, password, options)
+
+  add_membership2lings_role_from_table(membership, :expert, table)
+
 end
 
 Given /^the following users:$/ do |table|
