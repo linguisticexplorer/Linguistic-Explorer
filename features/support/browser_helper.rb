33bd56a024dbc8193c543637c8d08b6ce015a707
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-11-09 18:43:00 -0500

    added new method in /browser_helper.rb

--- features/support/browser_helper.rb
+++ features/support/browser_helper.rb
@@ -2,3 +2,15 @@
 def accept_alert_popup
   page.driver.browser.switch_to.alert.accept
 end
+
+def is_alert_popup_present?
+  begin
+    page.driver.browser.switch_to.alert.present?
+  rescue Selenium::WebDriver::Error::NoSuchAlertError
+    false
+  end
+end
+
+def dismiss_alert_popup
+  page.driver.browser.switch_to.alert.dismiss
+end
