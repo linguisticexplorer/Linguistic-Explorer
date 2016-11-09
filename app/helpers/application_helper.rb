977bb471b424a65f4a4ffb772c08bde3d70f5e64
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-11-09 17:58:38 -0500

    created 2 different version of can_see? method for every cases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -135,6 +135,20 @@ module ApplicationHelper
     end
   end
 
+  #different actions for an item
+  def can_see_any?(actions, item)
+    actions.any? do |action|
+      can_see?(action, item)
+    end
+  end
+
+  #different actions for different items
+  def can_see_header?(actions, items)
+    items.any? do |item|
+      can_see_any?(actions, item)
+    end
+  end
+
   def can_see_some?
     get_user_role(current_group) === 'Expert'
   end
