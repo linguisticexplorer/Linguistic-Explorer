8dbc02e6518d22732164d66f0b4ff6cc81fcc6eb
hab278 <hab278@nyu.edu>
2016-12-08 20:29:07 -0500

    Add computed RSA key to capistrano

--- config/deploy/travisci.rb
+++ config/deploy/travisci.rb
@@ -2,9 +2,9 @@
 set :user     , "deploy"
 set :use_sudo , true
 
-set :ssh_options, {
-  :forward_agent => true
-}
+ssh_options_hash = { :forward_agent => true }
+ssh_options_hash[:keys] = ["tmp/deploy_rsa"] if File.exist?("tmp/deploy_rsa")
+set :ssh_options, ssh_options_hash
 
 server "ec2-54-68-27-245.us-west-2.compute.amazonaws.com", :app, :web, :db, :primary => true
 
